8b3752534052e15be3a3507ec11d1cfdf48e6ffb
terciolanger <terciolanger@gmail.com>
2023-04-05 16:38:51 -0300

    atualizar JavaScript

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade permitida na rodovia é: ${vel} Km/hora`)
+if (vel >= 60.5) {
+    console.log('Você está dentro da velocidade permitida!') 
+}   
+    else {  
+        console.log(`Você ultrapassou a velocidade permitida!`)
+    }
